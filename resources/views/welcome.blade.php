160fdaf3bafe0c05575572d15c76c3aaa9717f2d
Kjell Kind <kjellkind@gmail.com>
2017-11-04 17:13:12 +0100

    title laravel 5

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Welcom to Laravel 5
                 </div>
 
                 <div class="links">
